5be07102aeecf83575423a0af6ca7241186f4fc6
camerons11 <shaffercm@g.crookcountyschools.org>
2022-01-26 10:10:37 -0800

    armys

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 "use strict"
 console.log("hello")
-const warhammer40kgod=["nurgle khorne slaanesh tzeentch"]
-console.log(warhammer40kgod [0])
-warhammer40kgod.shift("nurgle")
+const warhammer40kgod=["nurgle","khorne","slaanesh","Tzeentch"];
+console.log(warhammer40kgod);
+warhammer40kgod.shift('nurgle');
+console.log(warhammer40kgod);
+warhammer40kgod.unshift('chaos');
+console.log(warhammer40kgod);
+const armys=["space marines","chaos marines","orks","eldar" ]
+document.write(armys);
